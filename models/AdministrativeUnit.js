84b523d108cf9fd6d0c85c883613fb5ad12eae45
joel-2112 <eyueljoel21@gmail.com>
2025-06-09 09:43:10 +0300

    the adminstrative unit model ehanced

--- models/AdministrativeUnit.js
+++ models/AdministrativeUnit.js
@@ -9,17 +9,48 @@ module.exports = (db, DataTypes) => {
         allowNull: false,
       },
       name: {
-        type: DataTypes.STRING,
+        type: DataTypes.STRING(100),
         allowNull: false,
         validate: {
-          notEmpty: true,
           len: [2, 100],
         },
       },
-      code: {
+      type: {
         type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isIn: [['Regiopolitan', 'Zone City', 'Woreda city', 'Meri','Newus','Tadagi']],
+        },
+      },
+      parent_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'AdministrativeUnits',
+          key: 'id',
+        },
+      },
+      code: {
+        type: DataTypes.STRING(20),
         unique: true,
         allowNull: true,
+        validate: {
+          len: [1, 20],
+        },
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
       },
     },
     {
