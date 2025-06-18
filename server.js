d3a0d53e8a4d7c7781ba7d35b82010b3ed0a7485
joel-2112 <eyueljoel21@gmail.com>
2025-06-18 11:51:21 +0300

    the igration is done by force true for now

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const startServer = async () => {
     await sequelize.authenticate();
     console.log('Database connected successfully');
     // Sync models with the database
-    await sequelize.sync({force:true});
+    await sequelize.sync();
     app.listen(port, () => {
       console.log(`Server running on port ${port}`);
     });
