958b95f197f064d04fc13c346c7bb78cebfff7f4
joel-2112 <eyueljoel21@gmail.com>
2025-10-16 17:11:52 +0300

    server backend added

--- models/User.js
+++ models/User.js
@@ -65,7 +65,7 @@ module.exports = (db, DataTypes) => {
       },
       role_id: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: true,
         references: { model: "roles", key: "id" },
       },
       administrative_unit_id: {
