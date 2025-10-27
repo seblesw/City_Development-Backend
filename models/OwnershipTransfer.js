719c7c771d7f656bddfba5d20da56b8ba2300284
joel-2112 <eyueljoel21@gmail.com>
2025-10-27 11:45:17 +0300

    documents property added to the model

--- models/OwnershipTransfer.js
+++ models/OwnershipTransfer.js
@@ -1,25 +1,29 @@
-
 const PROPERTY_USE = {
   RESIDENTIAL: "መኖሪያ",
-  MIXED: "ቅይጥ",
   ORGANIZATION: "ድርጅት",
+  MIXED: "ቅይጥ",
+};
+
+SALE_OR_GIFT_SUB = {
+  LEASE: "በሊዝ ይዞታ",
+  EXISTING: "በነባር ይዞታ",
 };
 
 const TRANSFER_TYPE = {
-  SALE_OR_GIFT: "sale_or_gift",
+  SALE_OR_GIFT: "በሽያጭ ወይም በስጦታ ስመ-ንብረት ዝውውር",
   BANK_FORECLOSURE: "በባንክ ሐራጅ የሚሸጥ ቤት እና ይዞታ ስመ-ንብረት ዝውውር",
-  COURT_DECISION: "በፍርድ ዉሳኔ የሚተላለፍ ቤት እና ይዞታ ስመ-ንብረት ዝውውር", 
+  COURT_DECISION: "በፍርድ ዉሳኔ የሚተላለፍ ቤት እና ይዞታ ስመ-ንብረት ዝውውር",
   CONDOMINIUM: "የጋራ ህንጻ ኮንዶሚኒየም ቤት ስመ-ንብረት ዝውውር",
   RESIDENTIAL_ASSOCIATION: "የመኖሪያ ቤት ህብረት ስራ ማህበር ቤት እና ይዞታ ስመ-ንብረት ዝውውር",
   INHERITANCE: "በውርስ የተገኘ ቤት እና ይዞታ ስመ-ንብረት ዝውውር",
   REALSTATE_HOUSE: "የሪልስቴት ቤት ስመ-ንብረት ዝውውር",
-  TRADE_ORGANIZATION: "የንግድ ማህበር አደረጃጀት ስመ-ንብረት ዝውውር"
+  TRADE_ORGANIZATION: "የንግድ ማህበር አደረጃጀት ስመ-ንብረት ዝውውር",
 };
 
 const INHERITANCE_RELATION = {
   CHILD: "ከልጅ ወደ ወላጅ",
-  PARENT: "ከወላጅ ወደ ልጅ", 
-  OTHER: "ሌላ"
+  PARENT: "ከወላጅ ወደ ልጅ",
+  OTHER: "ሌላ",
 };
 
 module.exports = (db, DataTypes) => {
@@ -30,21 +34,14 @@ module.exports = (db, DataTypes) => {
       primaryKey: true,
       allowNull: false,
     },
-    
+
     // Property Information Fields
-    property_use: {
-      type: DataTypes.STRING
-    },
     property_use: {
       type: DataTypes.ENUM(Object.values(PROPERTY_USE)),
       allowNull: true,
     },
     transfer_type: {
       type: DataTypes.ENUM(Object.values(TRANSFER_TYPE)),
-      allowNull: true,
-    },
-    transfer_sub_type: {
-      type: DataTypes.STRING,
       allowNull: true,
     },
     inheritance_relation: {
@@ -55,54 +52,52 @@ module.exports = (db, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: true,
     },
-    property_area: {
-      type: DataTypes.DECIMAL(10, 2),
+    parcel_number: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    land_area: {
+      type: DataTypes.DECIMAL,
       allowNull: true,
     },
     land_value: {
-      type: DataTypes.DECIMAL(15, 2),
+      type: DataTypes.DECIMAL,
       allowNull: true,
     },
-    building_value: {
-      type: DataTypes.DECIMAL(15, 2),
+    property_value: {
+      type: DataTypes.DECIMAL,
       allowNull: true,
     },
     property_location: {
       type: DataTypes.TEXT,
       allowNull: true,
     },
-    
+    file: {
+      type: DataTypes.JSONB,
+      allowNull: false,
+      defaultValue: [],
+    },
+
     // Fee Calculation Fields
     base_value: {
-      type: DataTypes.DECIMAL(15, 2),
+      type: DataTypes.DECIMAL,
       allowNull: true,
     },
     service_fee: {
-      type: DataTypes.DECIMAL(15, 2),
+      type: DataTypes.DECIMAL,
       allowNull: true,
     },
     tax_amount: {
-      type: DataTypes.DECIMAL(15, 2),
+      type: DataTypes.DECIMAL,
       allowNull: true,
     },
     total_payable: {
-      type: DataTypes.DECIMAL(15, 2),
+      type: DataTypes.DECIMAL,
       allowNull: true,
     },
-    service_label: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    tax_label: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    property_label: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    
+
     // Personal Information Fields
+    //transciever
     transceiver_full_name: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -119,6 +114,7 @@ module.exports = (db, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: true,
     },
+    //reciepent
     recipient_full_name: {
       type: DataTypes.STRING,
       allowNull: false,
