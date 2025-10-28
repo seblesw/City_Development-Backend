a9a4b62e8989e223ffbd213ef0b1a52b3330c085
joel-2112 <eyueljoel21@gmail.com>
2025-10-28 09:42:57 +0300

    ownership transfer mode added

--- models/OwnershipTransfer.js
+++ models/OwnershipTransfer.js
@@ -38,164 +38,224 @@ module.exports = (db, DataTypes) => {
         allowNull: false,
       },
 
-      // Property Information Fields
+      // Property Information
       property_use: {
         type: DataTypes.ENUM(Object.values(PROPERTY_USE)),
         allowNull: true,
+        validate: {
+          isIn: {
+            args: [Object.values(PROPERTY_USE)],
+            msg: "የንብረት አጠቃቀም ከተፈቀዱት እሴቶች ውስጥ መሆን አለበት።",
+          },
+        },
       },
       transfer_type: {
         type: DataTypes.ENUM(Object.values(TRANSFER_TYPE)),
         allowNull: true,
+        validate: {
+          isIn: {
+            args: [Object.values(TRANSFER_TYPE)],
+            msg: "የስመ-ንብረት አይነት ከተፈቀዱት እሴቶች ውስጥ መሆን አለበት።",
+          },
+        },
       },
       inheritance_relation: {
         type: DataTypes.ENUM(Object.values(INHERITANCE_RELATION)),
         allowNull: true,
+        validate: {
+          isIn: {
+            args: [Object.values(INHERITANCE_RELATION)],
+            msg: "የውርስ ግንኙነት ከተፈቀዱት እሴቶች ውስጥ መሆን አለበት።",
+          },
+        },
       },
       plot_number: {
         type: DataTypes.STRING,
         allowNull: true,
+        validate: {
+          len: { args: [0, 50], msg: "የፕሎት ቁጥር ከ50 ቁምፊዎች መብለጥ አይችልም።" },
+        },
       },
       parcel_number: {
         type: DataTypes.STRING,
         allowNull: true,
+        validate: {
+          len: { args: [0, 50], msg: "የፓርሴል ቁጥር ከ50 ቁምፊዎች መብለጥ አይችልም።" },
+        },
       },
       land_area: {
         type: DataTypes.DECIMAL(15, 2),
         allowNull: true,
+        validate: {
+          min: { args: [0], msg: "የመሬት ስፋት ከ0 በታች መሆን አይችልም።" },
+        },
       },
       land_value: {
         type: DataTypes.DECIMAL(15, 2),
         allowNull: true,
+        validate: {
+          min: { args: [0], msg: "የመሬት ዋጋ ከ0 በታች መሆን አይችልም።" },
+        },
       },
-      property_value: {
+      building_value: {
         type: DataTypes.DECIMAL(15, 2),
         allowNull: true,
+        validate: {
+          min: { args: [0], msg: "የህንፃ ዋጋ ከ0 በታች መሆን አይችልም።" },
+        },
       },
       property_location: {
         type: DataTypes.TEXT,
         allowNull: true,
-      },
-      file: {
-        type: DataTypes.JSONB,
-        allowNull: false,
-        defaultValue: [],
+        validate: {
+          len: { args: [0, 500], msg: "የንብረት አድራሻ ከ500 ቁምፊዎች መብለጥ አይችልም።" },
+        },
       },
 
-      // Fee Calculation Fields
+      // Fee Calculation
       base_value: {
         type: DataTypes.DECIMAL(15, 2),
         allowNull: true,
+        validate: {
+          min: { args: [0], msg: "መሠረታዊ ዋጋ ከ0 በታች መሆን አይችልም።" },
+        },
       },
       service_fee: {
         type: DataTypes.DECIMAL(15, 2),
         allowNull: true,
+        validate: {
+          min: { args: [0], msg: "የአገልግሎት ክፍያ ከ0 በታች መሆን አይችልም።" },
+        },
       },
       service_rate: {
         type: DataTypes.DECIMAL(5, 2),
         allowNull: true,
+        validate: {
+          min: { args: [0], msg: "የአገልግሎት ተመን ከ0 በታች መሆን አይችልም።" },
+          max: { args: [100], msg: "የአገልግሎት ተመን ከ100 በላይ መሆን አይችልም።" },
+        },
       },
       tax_amount: {
         type: DataTypes.DECIMAL(15, 2),
         allowNull: true,
+        validate: {
+          min: { args: [0], msg: "የታክስ መጠን ከ0 በታች መሆን አይችልም።" },
+        },
       },
       tax_rate: {
         type: DataTypes.DECIMAL(5, 2),
         allowNull: true,
+        validate: {
+          min: { args: [0], msg: "የታክስ ተመን ከ0 በታች መሆን አይችልም።" },
+          max: { args: [100], msg: "የታክስ ተመን ከ100 በላይ መሆን አይችልም።" },
+        },
       },
       total_payable: {
         type: DataTypes.DECIMAL(15, 2),
         allowNull: true,
+        validate: {
+          min: { args: [0], msg: "ጠቅላላ ክፍያ ከ0 በታች መሆን አይችልም።" },
+        },
       },
 
-      // Personal Information Fields - Transceiver
+      // Personal Information - Transceiver
       transceiver_full_name: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notEmpty: { msg: "የተጋራቢ ሙሉ ስም ባዶ መሆን አይችልም።" },
+          len: { args: [1, 100], msg: "የተጋራቢ ስም ከ100 ቁምፊዎች መብለጥ አይችልም።" },
+        },
       },
       transceiver_phone: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notEmpty: { msg: "የተጋራቢ ስልክ ቁጥር ባዶ መሆን አይችልም።" },
+          len: { args: [1, 20], msg: "የተጋራቢ ስልክ ቁጥር ከ20 ቁምፊዎች መብለጥ አይችልም።" },
+        },
       },
       transceiver_email: {
         type: DataTypes.STRING,
         allowNull: true,
+        validate: {
+          isEmail: { msg: "የተጋራቢ ኢሜይል ትክክለኛ መሆን አለበት።" },
+        },
       },
       transceiver_nationalid: {
         type: DataTypes.STRING,
         allowNull: true,
+        validate: {
+          len: { args: [0, 50], msg: "የተጋራቢ መለያ ቁጥር ከ50 ቁምፊዎች መብለጥ አይችልም።" },
+        },
       },
 
-      // Personal Information Fields - Recipient
+      // Personal Information - Recipient
       recipient_full_name: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notEmpty: { msg: "የተቀባይ ሙሉ ስም ባዶ መሆን አይችልም።" },
+          len: { args: [1, 100], msg: "የተቀባይ ስም ከ100 ቁምፊዎች መብለጥ አይችልም።" },
+        },
       },
       recipient_phone: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notEmpty: { msg: "የተቀባይ ስልክ ቁጥር ባዶ መሆን አይችልም።" },
+          len: { args: [1, 20], msg: "የተቀባይ ስልክ ቁጥር ከ20 ቁምፊዎች መብለጥ አይችልም።" },
+        },
       },
       recipient_email: {
         type: DataTypes.STRING,
         allowNull: true,
+        validate: {
+          isEmail: { msg: "የተቀባይ ኢሜይል ትክክለኛ መሆን አለበት።" },
+        },
       },
       recipient_nationalid: {
         type: DataTypes.STRING,
         allowNull: true,
+        validate: {
+          len: { args: [0, 50], msg: "የተቀባይ መለያ ቁጥር ከ50 ቁምፊዎች መብለጥ አይችልም።" },
+        },
       },
 
       // Administrative Unit Reference
       administrative_unit_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
-        references: {
-          model: "administrative_units",
-          key: "id",
-        },
       },
+
+      // User References
       created_by: {
         type: DataTypes.INTEGER,
         allowNull: true,
-        references: {
-          model: "users",
-          key: "id",
-        },
       },
       updated_by: {
         type: DataTypes.INTEGER,
         allowNull: true,
-        references: {
-          model: "users",
-          key: "id",
-        },
       },
+
     },
     {
       tableName: "ownership_transfers",
       timestamps: true,
       paranoid: true,
       freezeTableName: true,
-
       indexes: [
-        {
-          fields: ["administrative_unit_id"],
-        },
-        {
-          fields: ["transfer_type"],
-        },
-        {
-          fields: ["property_use"],
-        },
-        {
-          fields: ["status"],
-        },
-        {
-          fields: ["createdAt"],
-        },
+        { fields: ["administrative_unit_id"] },
+        { fields: ["transfer_type"] },
+        { fields: ["property_use"] },
+        { fields: ["created_by"] },
+        { fields: ["updated_by"] },
+        { fields: ["createdAt"] },
       ],
     }
   );
 
-  // Return both the model and constants as an object
+  // Return both model and constants as object
   return {
     OwnershipTransfer,
     SALE_OR_GIFT_SUB,
